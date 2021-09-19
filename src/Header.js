6f974f575bb63c21f62b884aec9b3cf3f333e2a8
hannahk0108 <hannahk0108@gmail.com>
2021-09-18 21:07:11 -0500

    amazon navbar partially completed

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+//import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 export default function Header() {
   return (
@@ -8,8 +11,8 @@ export default function Header() {
       <img className="header__logo" alt="" src="/img/amazon_white.png" />
       {/* Seach Bar */}
       <div className="header__search">
-        <input className="header__searcgInput" type="text" />
-        {/* Logo */}
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
       {/* Nav */}
       <div className="header__nav">
@@ -24,6 +27,11 @@ export default function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          {/* <ShoppingBasketIcon /> */}
+          <ShoppingCartOutlinedIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
